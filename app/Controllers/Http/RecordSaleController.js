bddcaf8c7eff05993e157027d25aae97ab4f6a9d
Yury Cavalcante <yurypcfarias@gmail.com>
2019-05-13 16:36:44 -0300

    Added new properties and created ne methods

--- app/Controllers/Http/RecordSaleController.js
+++ app/Controllers/Http/RecordSaleController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const RecordSale = use('App/Models/RecordSale')
+const Record = use('App/Models/Record')
 
 class RecordSaleController {
   async index ({}) {
@@ -17,9 +18,25 @@ class RecordSaleController {
       let record_sale = data[i]
       record_sale.sale_id = params.id
       results.push(record_sale);
-    } 
 
-    
+      const record = await Record.findByOrFail('id', data[i].record_id)
+      try {
+        const downStock = record.stock - data[i].quantity
+        if(downStock < 0) {
+          console.log(downStock);
+          throw "Unable to lower stock, it reached 0 and its going to be negative"
+        } else {
+          record.merge({ stock: downStock })
+          await record.save()
+          if(downStock === 0) {
+            console.log("Warning: stock reached 0!")
+          }
+        }
+      } catch (error) {
+        console.log('Unhandled error on lowering stock' + error)
+      }
+    }
+
     const record_sales = await RecordSale.createMany(results);
 
     return record_sales
--- app/Controllers/Http/SaleController.js
+++ app/Controllers/Http/SaleController.js
@@ -11,11 +11,39 @@ class SaleController {
 
   async store ({ request, auth }) {
     const { id } = auth.user
-    const data = request.only(['total'])
+    const data = request.only(['total', 'cep', 'shipping_value', 'tracking_code'])
 
     const sale = Sale.create({ ...data, user_id: id, status: 'sold'})
 
     return sale;
+  }
+
+  async show ({ params }) {
+    const sale = await Sale.findOrFail(params.id)
+
+    await sale.load('record_sales')
+
+    return sale
+  }
+
+  async update ({ params, request }) {
+    const sale = await Sale.findOrFail(params.id)
+
+    const data = request.only(['total', 'cep', 'shipping_value', 'tracking_code'])
+
+    sale.merge(data)
+
+    await sale.save()
+
+    return sale
+  }
+
+  async destroy ({ params, response }) {
+    const sale = await Sale.findOrFail(params.id)
+
+    sale.delete()
+
+    return response.json({ message: "Sale deleted succesfully" })
   }
 }
 
